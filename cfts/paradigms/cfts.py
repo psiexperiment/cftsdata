323362fa3b4b3ad3b768b340b4b6b4e58ce9782d
Brad Buran <buran@ohsu.edu>
2022-10-13 07:41:05 -0700

    Misc bugfixes to fix paths after psiexperiment -> cfts refactor

--- cfts/paradigms/cfts.py
+++ cfts/paradigms/cfts.py
@@ -1,9 +1,36 @@
 from psi.experiment.api import ParadigmDescription
-from .core import CORE_PATH, microphone_mixin, microphone_fft_mixin
 
 
 CAL_PATH = 'psi.paradigms.calibration.'
-CFTS_PATH = 'psi.paradigms.cfts.'
+CFTS_PATH = 'cfts.paradigms.'
+CORE_PATH = 'psi.paradigms.core.'
+
+
+microphone_mixin = {
+    'manifest': CORE_PATH + 'signal_mixins.SignalViewManifest',
+    'attrs': {
+        'id': 'microphone_signal_view',
+        'title': 'Microphone view (time)',
+        'time_span': 4,
+        'time_delay': 0.125,
+        'source_name': 'microphone',
+        'y_label': 'Microphone (dB)'
+    },
+}
+
+
+microphone_fft_mixin = {
+    'manifest': CORE_PATH + 'signal_mixins.SignalFFTViewManifest',
+    'attrs': {
+        'id': 'microphone_fft_view',
+        'title': 'Microphone view (PSD)',
+        'fft_time_span': 0.25,
+        'fft_freq_lb': 5,
+        'fft_freq_ub': 64000,
+        'source_name': 'microphone',
+        'y_label': 'Microphone (dB)'
+    }
+}
 
 
 eeg_mixin = {
