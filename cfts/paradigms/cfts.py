40ce016459a37b96d8758a713bdc5d74d99899ea
Brad Buran <bburan@alum.mit.edu>
2022-10-20 08:03:02 -0700

    Fix y-axis label on microphone plots

--- cfts/paradigms/cfts.py
+++ cfts/paradigms/cfts.py
@@ -10,11 +10,11 @@ microphone_mixin = {
     'manifest': CORE_PATH + 'signal_mixins.SignalViewManifest',
     'attrs': {
         'id': 'microphone_signal_view',
-        'title': 'Microphone view (time)',
+        'title': 'Microphone (time)',
         'time_span': 4,
         'time_delay': 0.125,
         'source_name': 'microphone',
-        'y_label': 'Microphone (dB)'
+        'y_label': 'Microphone (V)'
     },
 }
 
@@ -23,7 +23,7 @@ microphone_fft_mixin = {
     'manifest': CORE_PATH + 'signal_mixins.SignalFFTViewManifest',
     'attrs': {
         'id': 'microphone_fft_view',
-        'title': 'Microphone view (PSD)',
+        'title': 'Microphone (PSD)',
         'fft_time_span': 0.25,
         'fft_freq_lb': 5,
         'fft_freq_ub': 64000,
