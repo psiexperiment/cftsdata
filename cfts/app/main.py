184916928da2b1929babc4f75f78798367242f0c
Brad Buran <buran@ohsu.edu>
2023-01-23 11:09:57 -0800

    Add launcher for running CFTS experiments

---
+++ cfts/app/main.py
@@ -0,0 +1,16 @@
+from enaml.qt.qt_application import QtApplication
+import enaml
+with enaml.imports():
+    from .gui import Main
+
+
+from psi.application import load_paradigm_descriptions
+
+
+def cfts():
+    load_paradigm_descriptions()
+    app = QtApplication()
+    view = Main()
+    view.show()
+    app.start()
+    return True
