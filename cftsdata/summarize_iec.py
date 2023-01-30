c3003731fd696a34ee22f32cb54c936961a7a99b
Brad Buran <buran@ohsu.edu>
2023-01-30 11:23:58 -0800

    Working version of summarize_iec

--- cftsdata/summarize_iec.py
+++ cftsdata/summarize_iec.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 from psiaudio import util
+from psiaudio.calibration import FlatCalibration
+from psiaudio.stim import chirp
+
 from psi.data.io.api import Recording
 
-from .util import DatasetManager
+from .util import DATA_ROOT, DatasetManager, process_files
 
 
 class IEC(Recording):
@@ -11,22 +16,46 @@ class IEC(Recording):
     def __init__(self, base_path, setting_table='epoch_metadata'):
         super().__init__(base_path, setting_table)
 
-    def get_epochs(self, columns='auto', offset=0, extra_duration=5e-3):
+    def get_epochs(self, columns='auto', offset=0, extra_duration=5e-3, cb=None):
         signal = getattr(self, 'hw_ai')
         duration = self.get_setting('hw_ao_chirp_duration')
-        return signal.get_epochs(self.epoch_metadata, offset, duration+extra_duration)
+        return signal.get_epochs(self.epoch_metadata, offset,
+                                 duration+extra_duration, cb=cb)
 
 
-def summarize_iec(filename, reprocess=False):
+def summarize_iec(filename, cb, reprocess=False):
     manager = DatasetManager(filename)
     if not reprocess and manager.is_processed('chirp normalized SPL.pdf'):
         return
+    manager.clear()
+
+    cb(0)
     fh = IEC(filename)
 
-    epochs = fh.get_epochs()
+    epochs = fh.get_epochs(cb=cb)
     cal = fh.hw_ai.get_calibration()
     freq_start = fh.get_setting('hw_ao_chirp_start_frequency')
     freq_end = fh.get_setting('hw_ao_chirp_end_frequency')
+    duration = fh.get_setting('hw_ao_chirp_duration')
+    window = fh.get_setting('hw_ao_chirp_window')
+    equalize = fh.get_setting('hw_ao_chirp_equalize')
+    levels = fh.epoch_metadata['hw_ao_chirp_level'].unique()
+
+    if equalize:
+        raise ValueError('Cannot process equalized chirps')
+
+    stim_cal = FlatCalibration.as_attenuation()
+    waveforms = {}
+    for level in levels:
+        w = chirp(fh.hw_ai.fs, freq_start, freq_end, duration, level, stim_cal,
+                  window, equalize)
+        n_pad = epochs.shape[-1] - w.shape[-1]
+        waveforms[level] = np.pad(w, (0, n_pad))
+
+    index = pd.Index(waveforms.keys(), name='hw_ao_chirp_level')
+    waveforms = pd.DataFrame(waveforms.values(), index=index,
+                             columns=epochs.columns)
+    waveforms_psd_db = util.db(util.psd_df(waveforms, fs=fh.hw_ai.fs))
 
     grouping = epochs.index.names[:-1]
     epochs_mean = epochs.groupby(grouping).mean()
@@ -35,44 +64,104 @@ def summarize_iec(filename, reprocess=False):
     epochs_spl = cal.get_db(epochs_psd)
     epochs_spl_mean = epochs_spl.groupby(grouping).mean()
 
-    figure, ax = plt.subplots(1, 1, figsize=(11.5, 8))
+    figure, axes = plt.subplots(2, 3, figsize=(15, 8))
+
+    ax = axes[0, 0]
+    for index, waveform in waveforms.iterrows():
+        ax.plot(waveform.index * 1e3, waveform.values, label=f'{index}')
+    ax.set_title('Speaker')
+    ax.set_xlabel('Time (ms)')
+    ax.set_ylabel('Amplitude (V)')
+    ax.legend()
+
+    ax = axes[0, 1]
     for index, waveform in epochs_mean.iterrows():
         ax.plot(waveform.index * 1e3, waveform.values, label=f'{index}')
-    ax.set_title('Chirp waveform')
+    ax.set_title('Microphone')
     ax.set_xlabel('Time (ms)')
-    ax.set_ylabel('Microphone amplitude (V)')
+    ax.set_ylabel('Amplitude (V)')
     ax.legend()
-    figure.savefig(manager.get_proc_filename('chirp waveform.pdf'), bbox_inches='tight')
 
-    figure, ax = plt.subplots(1, 1, figsize=(11.5, 8))
+    padding = 0.5
+
+    ax = axes[1, 1]
     for index, psd in epochs_psd_db_mean.iterrows():
         ax.plot(psd.index, psd.values, label=f'{index}')
-    ax.axis(xmin=freq_start * 0.9, xmax=freq_end / 0.9)
+    ax.axis(xmin=freq_start * padding, xmax=freq_end / padding)
     ax.axvline(freq_start, ls=':', color='k')
     ax.axvline(freq_end, ls=':', color='k')
     ax.set_xscale('octave')
-    ax.set_title('Chirp PSD')
     ax.set_xlabel('Frequency (kHz)')
-    ax.set_ylabel('Microphone PSD (V)')
+    ax.set_ylabel('PSD (V)')
     ax.legend()
-    figure.savefig(manager.get_proc_filename('chirp PSD.pdf'), bbox_inches='tight')
 
-    figure, ax = plt.subplots(1, 1, figsize=(11.5, 8))
+    ax = axes[1, 0]
+    for index, psd in waveforms_psd_db.iterrows():
+        ax.plot(psd.index, psd.values, label=f'{index}')
+    ax.axis(xmin=freq_start * padding, xmax=freq_end / padding, ymin=-100)
+    ax.axvline(freq_start, ls=':', color='k')
+    ax.axvline(freq_end, ls=':', color='k')
+    ax.set_xscale('octave')
+    ax.set_xlabel('Frequency (kHz)')
+    ax.set_ylabel('PSD (V)')
+    ax.legend()
+
+    ax = axes[0, 2]
     for index, spl in epochs_spl_mean.iterrows():
         ax.plot(spl.index, spl.values, label=f'{index}')
-    ax.axis(xmin=freq_start * 0.9, xmax=freq_end / 0.9)
+    ax.axis(xmin=freq_start * padding, xmax=freq_end / padding)
     ax.axvline(freq_start, ls=':', color='k')
     ax.axvline(freq_end, ls=':', color='k')
     ax.set_xscale('octave')
-    ax.set_title('Chirp SPL')
+    ax.set_title('Calibration')
     ax.set_xlabel('Frequency (kHz)')
     ax.set_ylabel('Stim. level (dB SPL)')
     ax.legend()
-    figure.savefig(manager.get_proc_filename('chirp SPL.pdf'), bbox_inches='tight')
 
-    #figure.savefig(manager.get_proc_filename('chirp normalized SPL.pdf'), bbox_inches='tight')
+    norm_spl = epochs_spl_mean - waveforms_psd_db
+    ax = axes[1, 2]
+    for index, spl in norm_spl.iterrows():
+        ax.plot(spl.index, spl.values, label=f'{index}')
+    ax.axis(xmin=freq_start * padding, xmax=freq_end / padding, ymin=80, ymax=160)
+    ax.axvline(freq_start, ls=':', color='k')
+    ax.axvline(freq_end, ls=':', color='k')
+    ax.set_xscale('octave')
+    ax.set_xlabel('Frequency (kHz)')
+    ax.set_ylabel('Normalized stim. level (dB SPL @ 1Vrms)')
+    ax.legend()
+    ax.grid()
 
-    plt.show()
+    figure.tight_layout()
+    manager.save_fig(figure, 'calibration.pdf')
+
+    waveforms = pd.DataFrame({
+        'sig_out': waveforms.stack(),
+        'mic_in': epochs_mean.stack()
+    })
+    manager.save_dataframe(waveforms, 'waveforms.csv')
+
+    psd = pd.DataFrame({
+        'sig_out': waveforms_psd_db.stack(),
+        'mic_in': epochs_psd_db_mean.stack(),
+        'mic_spl': epochs_spl_mean.stack(),
+        'norm_spl': norm_spl.stack(),
+    })
+    manager.save_dataframe(psd, 'psd.csv')
+
+
+def process_folder(folder, reprocess=False):
+    process_files(files, cb='tqdm', reprocess=reprocess)
+
+
+def main_folder():
+    import argparse
+    parser = argparse.ArgumentParser('Summarize IEC files in folder')
+    parser.add_argument('-f', '--folder', type=str, help='Folder containing IEC data', default=DATA_ROOT)
+    parser.add_argument('--reprocess', action='store_true', help='Reprocess all data in folder')
+    args = parser.parse_args()
+    process_files(args.folder, '**/*inear_speaker_calibration_chirp*',
+                  summarize_iec, reprocess=args.reprocess)
+
 
 if __name__ == '__main__':
     filename = r'C:\Users\mmm\projects\psi1\data\data\20230123-112200 Sean B008-1 left test inear_speaker_calibration_chirp'
