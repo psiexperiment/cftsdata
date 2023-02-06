4404402505b613cdff3b9a22cca9517d612aec55
Brad Buran <buran@ohsu.edu>
2023-02-06 15:14:42 -0800

    Allow multiple configs to be loaded into exp launcher

--- cfts/app/main.py
+++ cfts/app/main.py
@@ -9,9 +9,16 @@ from psi.application import load_paradigm_descriptions
 
 
 def cfts():
+    import argparse
+    parser = argparse.ArgumentParser('cfts')
+    parser.add_argument('config')
+
+    args = parser.parse_args()
     load_paradigm_descriptions()
     app = QtApplication()
     view = ExpLauncherMain()
+
+    view.settings.load_config(args.config)
     view.show()
     app.start()
     return True
