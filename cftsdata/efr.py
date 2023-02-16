0d957005e74d77601f69d66ac7e1ccdf6858cc8c
Brad Buran <buran@ohsu.edu>
2023-02-16 13:50:59 -0800

    WIP: Stub out EFR analysis

--- cftsdata/efr.py
+++ cftsdata/efr.py
@@ -1,4 +1,4 @@
-from psi.data.io.api import Recording
+from psidata.api import Recording
 
 
 class EFR(Recording):
@@ -6,8 +6,19 @@ class EFR(Recording):
     def __init__(self, filename, setting_table='analyze_efr_metadata'):
         super().__init__(filename, setting_table)
 
-    def get_eeg_epochs(self, **kwargs):
-        pass
+    def _get_epochs(self, signal):
+        duration = self.get_setting('duration')
+        offset = 0
+        result = signal.get_epochs(self.analyze_efr_metadata, offset, duration)
+        return result.reset_index(['target_sam_tone_fc', 'target_sam_tone_fm'],
+                                  drop=True)
 
-    def get_mic_epochs(self, **kwargs):
-        pass
+    @property
+    def mic(self):
+        return self.system_microphone
+
+    def get_eeg_epochs(self):
+        return self._get_epochs(self.eeg)
+
+    def get_mic_epochs(self):
+        return self._get_epochs(self.mic)
