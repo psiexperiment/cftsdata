5db19322d0ff8abf85728b82043c53bdedea9616
Brad Buran <buran@ohsu.edu>
2026-05-06 09:48:21 -0700

    Bugfix for processing DPOAEs if event log missing

--- cftsdata/dpoae.py
+++ cftsdata/dpoae.py
@@ -92,10 +92,8 @@ class DPOAEFile(Recording):
 
     def __init__(self, base_path, setting_table='dpoae_store'):
         super().__init__(base_path, setting_table)
+        self.results = self._results()
 
-    @property
-    @lru_cache(maxsize=MAXSIZE)
-    def results(self):
         data = getattr(self, 'dpoae_store')
         data = data.rename(columns=dpoae_renamer)
         m = data['capture'].diff() == 0
@@ -105,14 +103,18 @@ class DPOAEFile(Recording):
         # Add in the start/stop time of the actual stimulus itself. The
         # ts_start and ts_end timestamps indicate what was captured for the
         # online analysis.
-        ts = self.event_log.query('event == "dpoae_start"')['timestamp'].values.tolist()
-        ts.append(self.system_microphone.duration)
-        ts_start = ts[:-1]
-        ts_end = ts[1:]
-        if len(ts_start) != len(data):
+        try:
+            ts = self.event_log.query('event == "dpoae_start"')['timestamp'].values.tolist()
+            ts.append(self.system_microphone.duration)
+            ts_start = ts[:-1]
+            ts_end = ts[1:]
+            if len(ts_start) != len(data):
                 raise ValueError('Mismatch between event log and DPOAE metadata')
-        data['dp_start'] = ts_start
-        data['dp_end'] = ts_end
+            data['dp_start'] = ts_start
+            data['dp_end'] = ts_end
+        except:
+            data['dp_start'] = data['ts_start']
+            data['dp_end'] = data['ts_end']
         return data
 
     def iter_segments(self, microphone='system_microphone'):
